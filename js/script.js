135396abb0fd69f3c88f4df38ad16be09bad29a6
mariamik31 <mariam.khutsiashvili@gmail.com>
2024-03-19 21:25:00 +0400

    com

--- js/script.js
+++ js/script.js
@@ -27,29 +27,14 @@ splide.mount();
 
 // 
 
-// let accordion = document.getElementsByClassName("accordion");
-// let i;
+let accordion = document.getElementsByClassName("accordion");
+let i;
 
-// for (i = 0; i < accordion.length; i++) {
-//   accordion[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-
-//     let panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+
+    let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
@@ -57,6 +42,21 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// const accordion = document.getElementsByClassName("accordion");
+// const i;
+
+// for (i = 0; i < accordion.length; i++) {
+//   accordion[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     const panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
 
 
 
